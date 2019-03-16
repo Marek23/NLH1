eca1e836ffbe91ad1f77a1b84e460335a803a8da
unknown <marek-s56@tlen.pl>
2019-03-16 11:44:52 +0100

    Przygotowanie do szukania błędu.

--- NLCTV.m
+++ NLCTV.m
@@ -1,30 +1,16 @@
-function NLCTV()
-
-clc; clear;
+clc;
+clear;
 
 mex main.c;
 
 images = dir('C:\MAREK\MAGISTERKA\Obrazy\imgmask\*.png');
 
-for image=1:length(images)
+sw  = 1;
+h   = 0.5;
+s_r = 10;
+p_r = 5;
 
-h =1;
-while h<8
-
-s_r = 3;
-while s_r <17
-
-p_r = 1;
-while p_r < s_r && p_r <13
-
-sw =2; %%mój parametr(gdy =1 ==> oryginalny algorytm)
-while sw <= 5
-
-clearvars -except image images h s_r p_r sw
-
-images(image).name
-
-f0=imread(['C:\MAREK\MAGISTERKA\Obrazy\imgmask\' images(image).name]);
+f0=imread('Wood.png');
 
 %figure; imagesc(f0); colormap(gray); axis off; axis equal;
 f0=double(f0);
@@ -40,7 +26,7 @@ N   = n+2*t_r;
 
 BrokenAreaColor=240;
 
-lamda=.01;sigma=5; %%parametry jak poprzednio
+lamda   =.01;sigma=5; %%parametry jak poprzednio
 
 kernel  = fspecial('gaussian',p_s,sigma);
 kernelk = fspecial('gaussian',P_S,sigma);
@@ -56,7 +42,7 @@ u0=f0;
 
 tic
 u0r = main(m,n,c,u0(:),...
-    M,N,h,p_s,kernel(:),...
+    M,N,h,p_s,kernel(:),...*+asdf
     P_S,kernelk(:),t_r,s_r,p_r,sw,phi(:),...
     PHI(:),s_s,lamda,f0(:));
 t = toc;
@@ -64,18 +50,4 @@ t = toc;
 u0 = reshape(u0r,[m,n,c]);
 %figure; imagesc(uint8(u0)); colormap(gray); axis off; axis equal;
 
-imwrite(uint8(u0),['C:\MAREK\MAGISTERKA\Obrazy\nlctvWeigtEditCrimMet\' images(image).name 's_r_' num2str(s_r) 'p_r' num2str(p_r) 'h_' num2str(h) 'sw_' num2str(sw) 't_' num2str(t) '.png']);
-
-sw=sw+1;
-end
-
-p_r=p_r+2;
-end
-
-s_r=s_r+2;
-end
-
-h=h+1;
-end
-
-end
+imwrite(uint8(u0),['test\'  's_r_' num2str(s_r) 'p_r' num2str(p_r) 'h_' num2str(h) 'sw_' num2str(sw) 't_' num2str(t) '.png']);
